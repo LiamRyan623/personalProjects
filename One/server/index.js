ce5f94670bab5d4d382a428f170ca9a0f571da2a
Liam Ryan <liamryan623@outlook.com>
2023-08-15 22:43:12 -0400

    Plugging away at the server. Got it up and running. Next create controllers and routes.

--- One/server/index.js
+++ One/server/index.js
@@ -3,15 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+const log = console.log;
 
 // Controllers
-const character = require("./controller/character.controller");
+;
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use("/characters", character);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
